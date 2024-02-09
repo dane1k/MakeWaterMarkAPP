991fa4ef4dd84501ee758f654af6ea67a8b7b9b5
dane1k <dan9.b0rdan@yandex.ru>
2024-02-09 18:25:41 +1300

    The app has started work.

--- MakeWaterMarkAPP/Form1.Designer.cs
+++ MakeWaterMarkAPP/Form1.Designer.cs
@@ -28,22 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
+            components = new System.ComponentModel.Container();
             bOpen = new Button();
             folderBrowserDialog1 = new FolderBrowserDialog();
             lvFiles = new ListView();
             FileName = new ColumnHeader();
+            bAddWatermark = new Button();
+            openFileDialog1 = new OpenFileDialog();
+            tbWaterMark = new TextBox();
+            bSave = new Button();
+            saveFileDialog1 = new SaveFileDialog();
+            bProcessing = new Button();
+            progressBar1 = new ProgressBar();
+            timer1 = new System.Windows.Forms.Timer(components);
+            folderSaveDialog = new FolderBrowserDialog();
             SuspendLayout();
-            // 
-            // button1
-            // 
-            button1.Location = new Point(491, 317);
-            button1.Name = "button1";
-            button1.Size = new Size(142, 49);
-            button1.TabIndex = 0;
-            button1.Text = "btn_addwatermarkTest";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
             // 
             // bOpen
             // 
@@ -60,7 +59,7 @@
             lvFiles.Columns.AddRange(new ColumnHeader[] { FileName });
             lvFiles.Location = new Point(12, 74);
             lvFiles.Name = "lvFiles";
-            lvFiles.Size = new Size(410, 349);
+            lvFiles.Size = new Size(327, 349);
             lvFiles.TabIndex = 2;
             lvFiles.UseCompatibleStateImageBehavior = false;
             lvFiles.View = View.Details;
@@ -70,25 +69,95 @@
             FileName.Text = "FileName";
             FileName.Width = 400;
             // 
+            // bAddWatermark
+            // 
+            bAddWatermark.Location = new Point(173, 21);
+            bAddWatermark.Name = "bAddWatermark";
+            bAddWatermark.Size = new Size(126, 30);
+            bAddWatermark.TabIndex = 3;
+            bAddWatermark.Text = "Add Watermark";
+            bAddWatermark.UseVisualStyleBackColor = true;
+            bAddWatermark.Click += bAddWatermark_Click;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            openFileDialog1.FileOk += openFileDialog1_FileOk;
+            // 
+            // tbWaterMark
+            // 
+            tbWaterMark.Location = new Point(340, 26);
+            tbWaterMark.Name = "tbWaterMark";
+            tbWaterMark.Size = new Size(336, 23);
+            tbWaterMark.TabIndex = 4;
+            // 
+            // bSave
+            // 
+            bSave.Location = new Point(713, 25);
+            bSave.Name = "bSave";
+            bSave.Size = new Size(75, 23);
+            bSave.TabIndex = 5;
+            bSave.Text = "Save";
+            bSave.UseVisualStyleBackColor = true;
+            bSave.Click += bSave_Click;
+            // 
+            // saveFileDialog1
+            // 
+            saveFileDialog1.FileOk += saveFileDialog1_FileOk;
+            // 
+            // bProcessing
+            // 
+            bProcessing.Location = new Point(369, 74);
+            bProcessing.Name = "bProcessing";
+            bProcessing.Size = new Size(75, 23);
+            bProcessing.TabIndex = 6;
+            bProcessing.Text = "Processing";
+            bProcessing.UseVisualStyleBackColor = true;
+            bProcessing.Click += bProcessing_Click;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(369, 115);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(405, 23);
+            progressBar1.TabIndex = 7;
+            progressBar1.Click += progressBar1_Click;
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(progressBar1);
+            Controls.Add(bProcessing);
+            Controls.Add(bSave);
+            Controls.Add(tbWaterMark);
+            Controls.Add(bAddWatermark);
             Controls.Add(lvFiles);
             Controls.Add(bOpen);
-            Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
-
-        private Button button1;
         private Button bOpen;
         private FolderBrowserDialog folderBrowserDialog1;
         private ListView lvFiles;
         private ColumnHeader FileName;
+        private Button bAddWatermark;
+        private OpenFileDialog openFileDialog1;
+        private TextBox tbWaterMark;
+        private Button bSave;
+        private SaveFileDialog saveFileDialog1;
+        private Button bProcessing;
+        private ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+        private FolderBrowserDialog folderSaveDialog;
     }
 }
--- MakeWaterMarkAPP/Form1.cs
+++ MakeWaterMarkAPP/Form1.cs
@@ -1,27 +1,31 @@
 using MyGraphic;
+using System.Diagnostics;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
 
 namespace MakeWaterMarkAPP
 {
 	public partial class Form1 : Form
 	{
 		List<string> fileNames = new();
+		string? watermarkName=null;
+		string? saveFilePath = null;
+
 		public Form1()
 		{
 			InitializeComponent();
+			bProcessing.Enabled = false;
 		}
-
-		private void button1_Click(object sender, EventArgs e)
+		private void CheckProcessing()
 		{
-			using (Image image = Image.FromFile(@"C:\TRYING\TESTS\Photoes\_1060261.jpg"))
+			if (fileNames.Count > 0 && watermarkName != null && saveFilePath != null)
 			{
-				using (Image watermarkImage = Image.FromFile(@"C:\TRYING\TESTS\Logo\logo.png"))
-				{
-					Image res = WatermarkLib.AddWatermark(image, watermarkImage);
-					res.Save(@"C:\TRYING\Export\_1060261WITHWATERMARK.jpg");
-
-				}
+				bProcessing.Enabled = true;
 			}
 		}
+
 
 		private void bOpen_Click(object sender, EventArgs e)
 		{
@@ -32,10 +36,93 @@ namespace MakeWaterMarkAPP
 
 				fileNames = files.ToList();
 
-
-
 				lvFiles.Items.AddRange(fileNames.ConvertAll(x => new ListViewItem(x)).ToArray());
 			}
+			CheckProcessing();
+		}
+
+		private void bAddWatermark_Click(object sender, EventArgs e)
+		{
+			if (openFileDialog1.ShowDialog() == DialogResult.OK)
+			{
+				
+			}
+			CheckProcessing();
+		}
+
+		private void openFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+		{
+			watermarkName = (sender as FileDialog).FileName;
+			tbWaterMark.Text = watermarkName;
+		}
+
+
+
+		private void bProcessing_Click(object sender, EventArgs e)
+		{
+			//timer1.Start();
+
+			int counter = 0;
+			progressBar1.Value = 0;
+			progressBar1.Maximum = fileNames.Count;
+
+			foreach (string fileName in fileNames)
+			{
+				using (Image image = Image.FromFile(fileName))
+				{
+					using (Image watermarkImage = Image.FromFile(watermarkName))
+					{
+						Image res = WatermarkLib.AddWatermark(image, watermarkImage);
+
+						var myImage = new MyImage();
+						myImage.FileName = fileName;
+						myImage.Image = res;
+						string filePathName = Path.GetFileName(fileName);
+						string finalSave = Path.Combine(saveFilePath, filePathName);
+                        using (Bitmap bmp = new Bitmap(myImage.Image, myImage.Image.Width, myImage.Image.Height))
+                        {
+							bmp.Save(finalSave);
+                        }
+                        //myImage.Image.Save(finalSave);
+					}
+				}
+				progressBar1.Value = counter++;
+			}
+            Process.Start(Environment.GetEnvironmentVariable("WINDIR") +  @"\explorer.exe", saveFilePath);
+            //MessageBox.Show("Processing completed!");
+            // timer1.Stop();
+            //progressBar1.Value = 0;
+        }
+		private void bSave_Click(object sender, EventArgs e)
+		{
+			DialogResult result = folderSaveDialog.ShowDialog();
+			if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(folderSaveDialog.SelectedPath))
+			{
+				saveFilePath = folderSaveDialog.SelectedPath;
+				// Process.Start(saveFilePath);
+			}
+            CheckProcessing();
+
+        }
+        // string destinationPath = Path.Combine(destinationFolder, $"{Path.GetFileNameWithoutExtension(image.FileName)}_WITHWATERMARK.png");
+
+        private void timer1_Tick(object sender, EventArgs e)
+		{
+			progressBar1.Increment(1);
+		}
+		private void saveFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+		{
+
+		}
+		private void progressBar1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		public class MyImage
+		{
+			public string FileName { get; set; }
+			public Image Image { get; set; }
 		}
 	}
 }
